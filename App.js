45ce7e4b66815dfe6d7c1c47d20214efad217918
HanJin Park <phj9020@hotmail.com>
2020-08-31 14:58:20 +0900

    state added

--- App.js
+++ App.js
@@ -6,14 +6,18 @@ import * as Location from "expo-location";
 import { Alert } from "react-native";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true,
+  }
+
   getLocation = async () => {
     try {
-      const response = await Location.requestPermissionsAsync();
-      console.log(response);
-      const location = await Location.getCurrentPositionAsync();
-      console.log(location);
+      await Location.requestPermissionsAsync();
+      const {coords :{latitude, longitude}} = await Location.getCurrentPositionAsync();
+      // Send to API and get Weather 
+      this.setState({isLoading : false});
     }catch(error){
-      Alert.alert("Cant find you")
+      Alert.alert("Cant find you", "so sad")
       }
     };
     
@@ -22,6 +26,7 @@ export default class extends React.Component {
     this.getLocation();
   };
   render() {
-    return <Loading />;
+    const {isLoading} = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
